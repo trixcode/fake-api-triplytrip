1a1a99fe7bbdd67b7f9c2e3e16387c9bd2c0635b
Randomir <chingizkg@gmail.com>
2019-04-18 19:02:52 +0600

    Added script for reading json db files

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 const jsonServer = require('json-server')
 const path = require('path')
 const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'src', 'db', 'db.json'))
-const middlewares = jsonServer.defaults()
+const fs = require('fs')
+const _ = require('lodash')
 
+const FIRST_OBJECT_KEY = 0
 const PORT = 3000;
+
+
+let endpoints = [];
+let objectDB = {};
+const isJson = (str) => {
+  try {
+      JSON.parse(str);
+  } catch (e) {
+      return false;
+  }
+  return true;
+}
+
+let files = fs.readdirSync(path.resolve(__dirname, './src/db/'))
+files.forEach((fileName) => {
+  if (fileName.indexOf('.json') > -1) {
+    const jsonObject = JSON.parse(fs.readFileSync('./src/db/' + fileName));
+
+    if( isJson(fs.readFileSync('./src/db/' + fileName))) {
+      endpoints.push(Object.keys(jsonObject)[FIRST_OBJECT_KEY]);
+      console.log('🗒    JSON file loaded : ' + fileName);
+      _.extend(objectDB, require(path.resolve(__dirname, './src/db/', fileName)));
+    }
+  }
+})
+
+const router = jsonServer.router(objectDB)
+const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use(router)
+
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`)
+  console.log('\n⛴    JSON Server is running at http://localhost:' + PORT );
+  for (var i = 0; i < endpoints.length; i++) {
+    console.info('🥁    Endpoint : http://localhost:' + PORT + '/' + endpoints[i]);
+  }
 })
